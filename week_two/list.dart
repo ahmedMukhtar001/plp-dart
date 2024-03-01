ba8b9a8f66df88a19ee8d34427501f556f64c4a8
ahmedMukhtar001 <kanzpanther@gmail.com>
2024-03-01 11:49:34 +0300

    lists

---
+++ week_two/list.dart
@@ -0,0 +1,15 @@
+void main(){
+  // LIST'
+
+  var numberList = [1,2,3,4,5];
+  print(numberList);
+  print(numberList[0]);
+  print(numberList[1]);
+  print(numberList[2]);
+  print(numberList[3]);
+  print(numberList[4]);
+
+  // Change an item
+  numberList[0] = 46;
+  print(numberList);
+}
