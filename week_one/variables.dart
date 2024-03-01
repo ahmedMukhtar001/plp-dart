212801a2b528c034e163c87bbe227383c61df4ff
ahmedMukhtar001 <kanzpanther@gmail.com>
2024-03-01 11:21:09 +0300

    first commit

---
+++ week_one/variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Variables in dart language
+  String school = "Raira university";
+  int total = 400;
+  double GPA = 3.5;
+  num year = 2024;
+  bool isMarried = true;
+
+  print(school);
+  print(total);
+  print(GPA);
+  print(year);
+  print(isMarried);
+}
---
+++ week_two/dataTypes.dart
@@ -0,0 +1,14 @@
+// Data types in Dart 
+
+void main() {
+  // Strings
+    String firstName = "Ahmed";
+    print("String: $firstName");
+  // Integers
+
+  // Doubles
+
+  // Booleans
+
+  // Dynamic
+}
