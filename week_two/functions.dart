6829b514893b293c0a4e1a361342f631e9913db4
ahmedMukhtar001 <kanzpanther@gmail.com>
2024-03-02 12:14:32 +0300

    addition and multiplication

--- week_two/functions.dart
+++ week_two/functions.dart
@@ -1,4 +1,28 @@
+// Addition
+int add(int a, int b) {
+  int sum = a + b;
+  return sum;
+}
+// Multiplication
+int multi(int c, int d){
+  int sum = c * d;
+  return sum;
+}
+
+
+
 void main(){
-    // functions
-    
+    // Addition
+    int num1 = 40;
+    int num2 = 50;
+
+    int total = add(num1, num2);
+    print("The sum is $total");
+
+    // Multiplication
+    int num3 = 2;
+    int num4 = 3;
+
+    int total2 = multi(num3, num4);
+    print("The multiplication is $total2");
 }
